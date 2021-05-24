a854f88bd13951df7084fc8c2de878ff553055d8
SudoBandit <g.wilson5@edu.salford.ac.uk>
2021-05-24 23:41:21 +0100

    comment fix
    grabs the entire comment ( version numbers a bit wonky)

--- Cobol2XML/src/cobol/CommentLineAssembler.java
+++ Cobol2XML/src/cobol/CommentLineAssembler.java
@@ -1,19 +1,34 @@
 package cobol;
+import java.util.ArrayList;
+
 import parse.*;
 import parse.tokens.*;
 public class CommentLineAssembler extends Assembler {
-/**
-* Pop a string, and set the target DataDivision to this
-* string.
-*
-* @param Assembly the assembly to work on
-*/
-public void workOn(Assembly a) {
-//System.out.println("commentLineAssembler");
-Cobol c = new Cobol();
-Token t = (Token) a.pop();
-if(t.sval() != null) {
-c.setCommentLine(t.sval().trim());
-a.setTarget(c); }
-}
+	/**
+	 * Pop a string, and set the target DataDivision to this
+	 * string.
+	 *
+	 * @param Assembly the assembly to work on
+	 */
+	public void workOn(Assembly a) {
+		//System.out.println("commentLineAssembler");
+		StringBuilder comment = new StringBuilder();
+		Cobol c = new Cobol();
+		System.out.println(a.toString());
+		comment.append(a.pop());
+		while (a.hasMoreElements()) {
+			comment.append(" ");
+			Token t = (Token) a.nextElement();
+			System.out.println(t);
+			if(t.sval() != null) {				
+				comment.append(t);
+			}
+			else
+				comment.append(t.nval());
+
+		}
+		String comments = comment.toString();
+		c.setCommentLine(comments);
+		a.setTarget(c);
+	}
 }
