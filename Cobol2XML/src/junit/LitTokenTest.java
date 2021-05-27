2fc227e59828ad720822b6ca87cc323104c4aef6
Thomas <T.Macgregor@edu.salford.ac.uk>
2021-05-27 16:49:15 +0100

    lit changes

--- Cobol2XML/src/junit/LitTokenTest.java
+++ Cobol2XML/src/junit/LitTokenTest.java
@@ -12,9 +12,36 @@ public class LitTokenTest {
 	public void test() {
 		
 		String a = "a";
-		Literal t1 = new Literal(a);
+		Literal t = new Literal(a);
 		Token i = new Token("a");
-		assertTrue(t1.qualifies(i));
+		
+		Integer b = 1;
+		Literal t1 = new Literal(b);
+		Token i1 = new Token(1);
+		
+		Double c = 1.05;
+		Literal t2 = new Literal(c);
+		Token i2 = new Token(1.05);
+		
+		String a1 = "a";
+		Literal t3 = new Literal(a1);
+		Token i3 = new Token("b");
+		
+		Integer b1 = 1;
+		Literal t4 = new Literal(b1);
+		Token i4 = new Token(12);
+		
+		Double c1 = 1.05;
+		Literal t5 = new Literal(c1);
+		Token i5 = new Token(1.12);
+		
+		assertTrue(t.qualifies(i));
+		assertTrue(t1.qualifies(i1));
+		assertTrue(t2.qualifies(i2));
+		assertFalse(t3.qualifies(i3));
+		assertFalse(t4.qualifies(i4));
+		assertFalse(t5.qualifies(i5));
+
 
 	}
 }
